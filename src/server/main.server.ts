62d13b3e6d77742242469ec2d286eb2dc7aab8fc
cctrlz <malkuisi2005@gmail.com>
2025-08-07 00:28:51 -0400

    1

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,6 @@
-import { makeHello } from "shared/module";
+import { KnitServer as Knit } from "@rbxts/knit";
 
-print(makeHello("main.server.ts"));
+Knit.AddServices(script.Parent?.FindFirstChild("Services") as Instance);  
+Knit.Start().then(() => {
+    print("Server Started");
+});
